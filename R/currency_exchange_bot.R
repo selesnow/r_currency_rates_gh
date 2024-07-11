e228eac80f0704f8d19d2e25c273c722460885d0
Alexey Seleznev <selesnow@gmail.com>
2024-07-11 19:34:58 +0300

    Добавил возможность указать валюту

--- R/currency_exchange_bot.R
+++ R/currency_exchange_bot.R
@@ -3,11 +3,11 @@ library(jsonlite)
 library(telegram.bot)
 
 # Функция для получения курса валют
-get_exchange_rate <- function(api_key) {
+get_exchange_rate <- function(api_key, currency) {
   url <- paste0("https://api.exchangerate-api.com/v4/latest/USD?apikey=", api_key)
   response <- GET(url)
   data <- fromJSON(content(response, "text"))
-  return(data$rates$EUR)
+  return(data$rates[[currency]])
 }
 
 # Функция для отправки сообщения в Telegram
@@ -17,11 +17,12 @@ send_telegram_message <- function(bot_token, chat_id, message) {
 }
 
 # Получение переменных среды
-api_key <- Sys.getenv("EXCHANGE_RATE_API_KEY")
+api_key   <- Sys.getenv("EXCHANGE_RATE_API_KEY")
 bot_token <- Sys.getenv("TELEGRAM_BOT_TOKEN")
-chat_id <- Sys.getenv("TELEGRAM_CHAT_ID")
+chat_id   <- Sys.getenv("TELEGRAM_CHAT_ID")
+currency  <- "EUR"
 
 # Получение курса валют и отправка сообщения
-exchange_rate <- get_exchange_rate(api_key)
-message <- paste("Курс EUR к USD: ", exchange_rate)
+exchange_rate <- get_exchange_rate(api_key, currency)
+message <- paste("Курс", currency, "к USD:", exchange_rate)
 send_telegram_message(bot_token, chat_id, message)
